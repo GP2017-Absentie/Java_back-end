b84d7363485dd69a5ddc8b7c2bff702a639b22f4
Nick W <nickwindt@hotmail.nl>
2017-04-04 13:48:46 +0200

    getLessenByStudentId added

--- AbsentieSysteem/src/app/model/StudentModel.java
+++ AbsentieSysteem/src/app/model/StudentModel.java
@@ -3,8 +3,10 @@ package app.model;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
-import app.object.Persoon;
+import app.object.Docent;
+import app.object.Les;
 import app.object.Student;
 
 public final class StudentModel {
@@ -32,5 +34,39 @@ public final class StudentModel {
 	        }
 	        return null;
 	    }
+	 
+	 public static ArrayList<Les> getLessenByStudentId(int id) {
+		 
+		 try {
+			 
+			 
+			 	ArrayList<Les> lessen = new ArrayList<Les>();
+			 
+	        	Statement stat = DatabaseModel.myConn.createStatement();
+	            ResultSet res = stat.executeQuery("SELECT `klas_FK` FROM `persoon` WHERE `id` = " + id); 
+	            res.next();
+	            
+	            ResultSet res2 = stat.executeQuery("SELECT * FROM `les` WHERE `klas_FK` = '" + res.getString("klas_FK") + "'"); 
+	            
+	            while (res2.next()) {
+	            	lessen.add(LesModel.getById(res2.getInt("id")));
+	            }
+            
+	            res.close();
+	            res2.close();
+	            stat.close();
+	            
+	            return lessen;
+
+	        } catch (SQLException ex) {
+	            // handle any errors
+	            System.out.println("SQLException: " + ex.getMessage());
+	            System.out.println("SQLState: " + ex.getSQLState());
+	            System.out.println("VendorError: " + ex.getErrorCode());
+	        }
+	        return null;
+		 
+		 
+	 }
 
 }
