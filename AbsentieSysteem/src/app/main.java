94acdac20ef203844a98ece5caa97e5a266ffb98
Owen Ashby <owenashby@hotmail.nl>
2017-04-05 12:11:48 +0200

    easier to test

--- AbsentieSysteem/src/app/main.java
+++ AbsentieSysteem/src/app/main.java
@@ -1,23 +1,36 @@
 package app;
 
-import app.model.AbsentieModel;
 import app.model.DatabaseModel;
+import app.model.DocentModel;
+import app.model.KlasModel;
 import app.model.LesModel;
 import app.model.PersoonModel;
+import app.model.StudentModel;
 
-class main
-{
+public class main {
     public static void main(String [] args) {
-        DatabaseModel.open();
-        //PersoonModel.getById(1);
-        //Absentie a1 = new Absentie()
-        LesModel.getById(1);
-        //PersoonModel.getById(8);
-        //AbsentieModel.getById(1);
-        //LesModel.getById(23);
-
-
-
+//        DatabaseModel.open();
+//        PersoonModel.getById(1);
+//        
+//        DatabaseModel.open();
+//        LesModel.getById(1);
+//        
+//        DatabaseModel.open();
+//        KlasModel.getListStudent("V1D");
+//    	
+//    	DatabaseModel.open();
+//    	KlasModel.getLessenKlas("V1D");
+//    	
+//    	DatabaseModel.open();
+//    	DocentModel.getLessenDocent(3);
+//    	
+//    	DatabaseModel.open();
+//    	StudentModel.getAbsentiesStudent(1676955);
+//    	
+//    	DatabaseModel.open();
+//    	DocentModel.getAbsentiesDocent(2);
+    	
+    	DatabaseModel.open();
+    	LesModel.getStudentenByLes(1);
     }
-
 }
