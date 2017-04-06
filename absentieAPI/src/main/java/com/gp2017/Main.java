e4b57d8c1e43ff4fcdf2de0157433afa30d93701
Owen Ashby <owenashby@hotmail.nl>
2017-04-06 11:42:46 +0200

    testtest

--- absentieAPI/src/main/java/com/gp2017/Main.java
+++ absentieAPI/src/main/java/com/gp2017/Main.java
@@ -1,5 +1,6 @@
 package com.gp2017;
 
+import com.gp2017.Model.AbsentieModel;
 import com.gp2017.Model.DatabaseModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +9,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 class Main
 {
     public static void main(String [] args) {
+//        DatabaseModel.open();
+//        SpringApplication.run(Main.class, args);
+//
         DatabaseModel.open();
-        SpringApplication.run(Main.class, args);
-
+        AbsentieModel absentieModel = new AbsentieModel();
+        absentieModel.getById(1);
     }
-
-
+//
+//
 }
